404613bd3d22c1108302d049b56dc8e0388cb90e
waiting <539959237@qq.com>
2020-03-09 23:42:22 +0800

    deploy

---
+++ precache-manifest.c130ae3708acf9ea90c716622672c8ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2e7c5ab5eb986299379",
+    "url": "/mone-ts-website/css/app.a97faad0.css"
+  },
+  {
+    "revision": "df9b883d2785b4a03b3dacf30b1e1be9",
+    "url": "/mone-ts-website/index.html"
+  },
+  {
+    "revision": "c2e7c5ab5eb986299379",
+    "url": "/mone-ts-website/js/app.dd0c1b8d.js"
+  },
+  {
+    "revision": "29d737567e7df4e85074",
+    "url": "/mone-ts-website/js/chunk-vendors.dcaf8a96.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/mone-ts-website/manifest.json"
+  },
+  {
+    "revision": "b21c2bfe449fe4c5335c9b0fab63bbd4",
+    "url": "/mone-ts-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mone-ts-website/robots.txt"
+  }
+]);
